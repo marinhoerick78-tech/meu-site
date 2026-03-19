ba86abe4ba7e4015fb32b8da16a1d2212fc09fda
marinhoerick78-tech <marinhoerick78@gmail.com>
2026-03-19 01:52:13 -0300

    Delete meu-site directory

--- meu-site/script.js
+++
@@ -1,5 +0,0 @@
- const button = document.querySelector("button");
- button.addEventListener("click", function() {
-  alert("Você clicou no botão!");
-  document.querySelector("p").textContent = "Você clicou!";
-});
